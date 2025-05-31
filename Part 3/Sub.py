022d36648452c249f09e19bf60214f4a08244dc8
Kareem T <77555690+kareemrt@users.noreply.github.com>
2025-05-30 18:41:32 -0700

    COMPLETE P3 Submission
    Complete code and structure.

--- Part 3/Sub.py
+++ Part 3/Sub.py
@@ -3,82 +3,68 @@
 import json, os, datetime
 from concurrent import futures
 from google.cloud import pubsub_v1
-from google.oauth2 import service_account
+from google.oauth2.service_account import Credentials
 import threading
 import libs.load as load
-# Global Variables
-# ---Important directories/files
-CURRENT_DIR = os.path.dirname(__file__)
-PARENT_DIR = os.path.dirname(CURRENT_DIR)
-PUBSUB_ACCOUNT_FILE = os.path.join(PARENT_DIR, "key_pubsub.json") # path to service account key file
-VEHICLE_IDS_FILE = os.path.join(PARENT_DIR, "vehicles.txt") # path to vehicle IDs file
-OUTPUT_FILE = os.path.join(CURRENT_DIR, f"crumbs/breadcrumbs_{datetime.date.today().isoformat()}.json")
-print(f'Output file path: {OUTPUT_FILE}')
-# ---Data
-breadcrumbs = []
-message_count = 0
-undelivered_messages = 0
-lock = threading.Lock() # Lock for thread safety
+
+# Constants
+PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
+KEY_FILE = os.path.join(PARENT_DIR, "key_pubsub.json") # path to service account key file
+VEHICLE_IDS_FILE = '/srv/DataEng/vehicles.txt' # path to vehicle IDs file
+OUTPUT_FILE = f"/srv/DataEng/Data/Breadcrumbs/{datetime.date.today().isoformat()}.json" # Output file path
+
+COUNT, UNDELIVERED, crumbs = 0, 0, [] # Globals
+
+lock = threading.Lock() # Lock for thread safety during file operations
 with open(VEHICLE_IDS_FILE, "r") as f: vehicle_ids = [bus.strip() for bus in f.readlines()] # Read vehicle IDs from file.
 
-# Callback function to process incoming messages
 def callback(message):
-    global breadcrumbs, message_count, undelivered_messages
+    '''Callback function to process incoming Pub/Sub messages.'''
+    global crumbs, COUNT, UNDELIVERED
     try:
         crumb = json.loads(message.data.decode("utf-8"))
-        with lock: breadcrumbs.append(crumb)
-        message_count += 1
-        undelivered_messages += 1
-        if message_count % 100000 == 0: 
-            print(f"Processed {message_count} messages")
-            with open(OUTPUT_FILE, "w") as f: 
-                with lock: 
-                    json.dump(breadcrumbs, f, indent=4)
-                    load.store_pubsub_data(breadcrumbs, 'breadcrumb')
-                undelivered_messages = 0
+        with lock: crumbs.append(crumb)
+        COUNT += 1
+        UNDELIVERED += 1
+        if UNDELIVERED % 100000 == 0: 
+            print(f"Processed {COUNT} messages")
+            with lock:
+                with open(OUTPUT_FILE, "w") as f: 
+                    json.dump(crumbs, f, indent=2)
+                    load.store_pubsub_data(crumbs, 'breadcrumb')
+                UNDELIVERED = 0
         message.ack()
     except Exception as e:
         print(f"Error processing message: {e}")
         message.nack()
 
 # (GCP) Google Cloud
-# ---Project and Subscription IDs
 project_id = "data-eng-scalzone"
-subscription_id = "breadcrumbs_sub"
-# ---Create Pub/Sub subscriber using credentials
-pubsub_creds =  (service_account.Credentials.from_service_account_file(PUBSUB_ACCOUNT_FILE))
-subscriber = pubsub_v1.SubscriberClient(credentials=pubsub_creds)
-subscription_path = subscriber.subscription_path(project_id, subscription_id)
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+sub_id = "breadcrumbs_sub"
+# Create subscriber using credentials
+sub = pubsub_v1.SubscriberClient(credentials=(Credentials.from_service_account_file(KEY_FILE)))
+sub_path = sub.subscription_path(project_id, sub_id)
+streaming_pull_future = sub.subscribe(sub_path, callback=callback)
 future_list = [] # This future_list will allow us to create a data structure to inspect in a blocking for loop shortly.
 
-# Ensure output file exists
-if not os.path.exists(OUTPUT_FILE): 
-    with open(OUTPUT_FILE, "w") as f: json.dump([], f)
-with open(OUTPUT_FILE, "r") as f: # Load existing content
-    try: breadcrumbs = json.load(f)
-    except json.JSONDecodeError: breadcrumbs = []
-
 # Infinite main loop to keep the subscriber running
-print(f"Listening for messages on {subscription_path}...")
+print(f"Listening for messages on {sub_path}...")
 while True:
-    OUTPUT_FILE = os.path.join(CURRENT_DIR, f"crumbs/breadcrumbs_{datetime.date.today().isoformat()}.json")
+    OUTPUT_FILE = f"/srv/DataEng/Data/Breadcrumbs/{datetime.date.today().isoformat()}.json" # Parse output file
+    with lock:
+        if not os.path.exists(OUTPUT_FILE): 
+            with open(OUTPUT_FILE, "w") as f: json.dump([], f) # Create output file if it doesn't exist.
+        with open(OUTPUT_FILE, "r") as f: crumbs = json.load(f) # Load already seen crumbs.
     try: 
-        message_count = 0
-        try:
-            streaming_pull_future.result(timeout = 100)
+        COUNT = 0
+        try: streaming_pull_future.result(timeout = 100)
         except futures.TimeoutError:
-            if undelivered_messages > 0:
-                print(f"Processed {message_count} messages with {undelivered_messages} undelivered.")
-                with open(OUTPUT_FILE, "w") as f: 
-                    with lock: 
-                        json.dump(breadcrumbs, f, indent=4)
-                        load.store_pubsub_data(breadcrumbs, 'breadcrumb')
-                undelivered_messages = 0
-            elif message_count > 0:
-                print(f"Processed {message_count} messages.")
-            else: 
-                print("No messages processed.")
-                breadcrumbs = []
+            if UNDELIVERED > 0:
+                with lock:
+                    with open(OUTPUT_FILE, "w") as f: 
+                        json.dump(crumbs, f, indent=2)
+                        load.store_pubsub_data(crumbs, 'breadcrumb')
+                print(f"Saved {UNDELIVERED} undelivered messages to {OUTPUT_FILE} and database.")
+                UNDELIVERED = 0
+        print(f'Processed {COUNT} messages. Output saved to {OUTPUT_FILE}.')
     except KeyboardInterrupt: streaming_pull_future.cancel()
-
